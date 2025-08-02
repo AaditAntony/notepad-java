38d0c7972c0e6aca3bc35cb4bc76db113d495656
Aadit Antony <aaditantony1112@gmail.com>
2025-08-02 12:53:07 +0530

    the note model has been completed and the correction in the user is also completed

---
+++ src/main/java/com/aadit/notepad_backend/model/Note.java
@@ -0,0 +1,32 @@
+package com.aadit.notepad_backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "notes")
+public class Note {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String tile;
+    @Column(columnDefinition = "TEXT")
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+}
